c22b2038967f77592a8f36c6c239bcc7ebb8dcc3
Fangwei Jiang <emberspirit2018@gmail.com>
2023-09-26 01:05:02 +1000

    update folder

--- react-state/src/components/Counter.jsx
+++
@@ -1,5 +0,0 @@
-import { useState } from "react";
-export const Counter = () => {
-    const [count, setCount] = useState(0);
-    return <p onClick={() => setCount(count => count + 1)}>current count number is: {count}</p>;
-};
