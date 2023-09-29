c2cb85e50005a751f72f4e78c2ac62f79721d377
Fangwei Jiang <emberspirit2018@gmail.com>
2023-09-29 16:59:19 +1000

    update UseEffect.jsx

---
+++ react-state & event listener/src/app/AppUseEffect.jsx
@@ -0,0 +1,41 @@
+import { useState, useEffect } from "react";
+
+export default function AppUseEffect() {
+    const [name, setName] = useState("");
+    const [age, setAge] = useState(0);
+    const [width, setWidth] = useState(window.innerWidth);
+
+    // useEffect(() => {
+    //     console.log(`age has changed to ${age}`);
+    // }, [age]);
+
+    useEffect(() => {
+        document.title = name;
+    }, [name]);
+
+    useEffect(() => {
+        window.addEventListener("resize", () => setWidth(window.innerWidth));
+    }, []);
+
+    useEffect(() => {
+        const handler = () => console.log(name);
+        document.addEventListener("click", handler);
+        return () => document.removeEventListener("click", handler);
+    }, [name]);
+
+    return (
+        <div>
+            <input value={name} onChange={e => setName(e.target.value)} />
+            <br />
+            <br />
+            <button onClick={() => setAge(age => age - 1)}>-</button>
+            <button onClick={() => setAge(age => age + 1)}>+</button>
+            <br />
+            <br />
+            My name is {name} and I am {age} years old.
+            <br />
+            <br />
+            {width}
+        </div>
+    );
+}
--- react-state & event listener/src/main.jsx
+++ react-state & event listener/src/main.jsx
@@ -4,6 +4,7 @@ import App from "./app/App";
 import AppClass from "./app/AppClass";
 import AppInput from "./app/AppInput";
 import AppArray from "./app/AppArray";
+import AppUseEffect from "./app/AppUseEffect";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
@@ -11,5 +12,6 @@ ReactDOM.createRoot(document.getElementById("root")).render(
         <AppClass />
         <AppInput />
         <AppArray />
+        <AppUseEffect />
     </React.StrictMode>
 );
