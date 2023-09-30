b79dc3ae255c73f6cee4791a35bf03fb7843a14d
Fangwei Jiang <emberspirit2018@gmail.com>
2023-10-01 01:27:41 +1000

    update ToDoList.jsx

--- userlist & todolist project/src/components/TodoList.jsx
+++ userlist & todolist project/src/components/TodoList.jsx
@@ -5,7 +5,7 @@ export function TodoList() {
     const [newTodoName, setNewTodoName] = useState(""); // restore input value
     const [todos, setTodos] = useState([]); // store list items
     const addNewTodo = () => newTodoName !== "" && (setTodos(currentTodos => [...currentTodos, { name: newTodoName, completed: false, id: crypto.randomUUID() }]), setNewTodoName("")); // create a new item and clear the input field
-    const toggleTodo = (todoId, completed) => setTodos(currentTodos => currentTodos.map(todo => (todo.id === todoId ? { ...todo, completed } : todo))); // only ensure the new item's complete attribute is set to false (default), keep rest of items unchanged
+    const toggleTodo = (todoId, completed) => setTodos(currentTodos => currentTodos.map(todo => todo.id === todoId && { ...todo, completed })); // only ensure the new item's complete attribute is set to false (default), keep rest of items unchanged
     const deleteTodo = todoId => setTodos(currentTodos => currentTodos.filter(todo => todo.id !== todoId)); // delete an item
     return (
         <>
