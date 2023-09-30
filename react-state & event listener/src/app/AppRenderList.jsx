93a80970ad58815cb5458cd0091b8e76fdeb8717
Fangwei Jiang <emberspirit2018@gmail.com>
2023-09-30 14:20:24 +1000

    userlist practice

--- react-state & event listener/src/app/AppRenderList.jsx
+++ react-state & event listener/src/app/AppRenderList.jsx
@@ -1,26 +1,26 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 export default function AppRenderList() {
     const [item, setItem] = useState([
         { id: crypto.randomUUID(), name: "Item 1" },
         { id: crypto.randomUUID(), name: "Item 2" },
     ]);
-
-    useEffect(() => {
-        if (item.length > 2) {
-            const newItem = item.at(-1);
-            alert(`${newItem.id} has been added.`);
-        }
-    }, [item]);
     return (
         <>
-            <button onClick={() => setItem(item => [...item, { id: crypto.randomUUID(), name: "New Item" }])}>add item</button>
+            <button
+                onClick={() => {
+                    alert(`${item.at(-1).id} has been added.`);
+                    setItem(item => [...item, { id: crypto.randomUUID(), name: "New Item" }]);
+                }}
+            >
+                add item
+            </button>
             <pre>
                 {item.map(item => {
                     return (
-                        <div key={item.id}>
+                        <React.Fragment key={item.id}>
                             <div>{item.name}</div>
                             <input type='text' />
-                        </div>
+                        </React.Fragment>
                     );
                 })}
             </pre>
---
+++ userlist project/src/components/UserList.jsx
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from "react";
+
+export function UserList() {
+    const [users, setUsers] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        const controller = new AbortController();
+        fetch("//jsonplaceholder.typicode.com/users", { signal: controller.signal })
+            .then(res => res.json())
+            .then(data => setUsers(data))
+            .catch(e => {
+                if (e?.name === "AbortError") return;
+                console.error(e);
+                setError("Error fetching data");
+            })
+            .finally(() => setLoading(false));
+        return () => controller.abort();
+    }, []);
+
+    let result;
+    if (loading) result = <h2>Loading</h2>;
+    else if (error) result = <h2>{error}</h2>;
+    else
+        result = (
+            <ul>
+                {users.map(user => (
+                    <li key={user.id}>{user.name}</li>
+                ))}
+            </ul>
+        );
+    return (
+        <>
+            <h1>User List</h1>
+            {result}
+        </>
+    );
+}
